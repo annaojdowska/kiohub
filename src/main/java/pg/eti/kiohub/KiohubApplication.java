fcbf1595e9a0b523cdd91fc3eedf8594c063de73
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-06-23 16:38:57 +0200

    Possible deploy as war

--- src/main/java/pg/eti/kiohub/KiohubApplication.java
+++ src/main/java/pg/eti/kiohub/KiohubApplication.java
@@ -2,6 +2,8 @@ package pg.eti.kiohub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class KiohubApplication {
@@ -9,4 +11,9 @@ public class KiohubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KiohubApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KiohubApplication.class);
+	}    
 }
