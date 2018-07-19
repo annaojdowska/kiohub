4ad9de7fb9a98701ddb6606d76336fd3aca9f3f7
jaskier07 <aleksanderkania96@gmail.com>
2018-07-19 22:49:21 +0200

    Angular - basic font styles

--- kiohub-client/src/app/content-container/content-container.component.ts
+++ kiohub-client/src/app/content-container/content-container.component.ts
@@ -5,8 +5,8 @@ selector: 'app-content-container',
 templateUrl: './content-container.component.html',
 styleUrls: ['./content-container.component.css']
 })
-
 export class ContentContainerComponent implements OnInit {
+
 
 ngOnInit() { }
 }
