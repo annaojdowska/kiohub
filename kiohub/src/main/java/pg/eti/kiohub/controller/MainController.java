8c0f9b372fc22ee44f79c651e1409ccb3788375d
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-07-23 22:30:12 +0200

    Change page main controller to index.html

--- kiohub/src/main/java/pg/eti/kiohub/controller/MainController.java
+++ kiohub/src/main/java/pg/eti/kiohub/controller/MainController.java
@@ -22,6 +22,6 @@ public class MainController {
 
         @RequestMapping(path = "/")
         public String homePage() {
-                return "homePage.html";
+                return "index.html";
         }
 }
