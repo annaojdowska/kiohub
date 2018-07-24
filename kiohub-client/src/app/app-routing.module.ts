cc2397272ccb835bad20d62de3200cd79cb79b60
Katarzyna <kstrzcinska@gmail.com>
2018-07-24 22:31:04 +0200

    Adding new project form

--- kiohub-client/src/app/app-routing.module.ts
+++ kiohub-client/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchInputComponent } from './search-input/search-input.component';
 import { UnloggedSearchComponent } from './unlogged-search/unlogged-search.component';
-
+import { AddProjectComponent } from './add-project/add-project.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: UnloggedSearchComponent }
+  { path: 'home', component: UnloggedSearchComponent },
+  { path: 'add-project', component: AddProjectComponent }
 ];
 
 
--- kiohub-client/src/app/app.module.ts
+++ kiohub-client/src/app/app.module.ts
@@ -5,16 +5,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-import { MatButtonModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 import { MenuButtonComponent } from './menu-button/menu-button.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MenuBarNotLoggedComponent } from './menu-bar-not-logged/menu-bar-not-logged.component';
 import { ContentContainerComponent } from './content-container/content-container.component';
 import { AppRoutingModule } from './/app-routing.module';
-
+import { FormsModule } from '@angular/forms';
 import { SearchService } from './search-service/search.service';
 import { SearchInputComponent } from 'src/app/search-input/search-input.component';
 import { UnloggedSearchComponent } from './unlogged-search/unlogged-search.component';
+import { AddProjectComponent } from './add-project/add-project.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { UnloggedSearchComponent } from './unlogged-search/unlogged-search.compo
     MenuButtonComponent,
     SearchInputComponent,
     ContentContainerComponent,
-    UnloggedSearchComponent
+    UnloggedSearchComponent,
+    AddProjectComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,9 @@ import { UnloggedSearchComponent } from './unlogged-search/unlogged-search.compo
     MatButtonModule,
     MatInputModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
   providers: [ SearchService ],
   bootstrap: [AppComponent]
