3a331ddf121c0f23849b5b44641c1f5034cfa72c
Katarzyna <kstrzcinska@gmail.com>
2018-07-25 22:26:04 +0200

    Form for edit project - general tab

--- kiohub-client/src/app/edit-project-general-tab/edit-project-general-tab.component.ts
+++ kiohub-client/src/app/edit-project-general-tab/edit-project-general-tab.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Status {
+  value: number;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-edit-project-general-tab',
   templateUrl: './edit-project-general-tab.component.html',
   styleUrls: ['./edit-project-general-tab.component.css']
 })
-export class EditProjectGeneralTabComponent implements OnInit {
 
+export class EditProjectGeneralTabComponent implements OnInit {
+  statuses: Status[] = [
+    {value: 0, viewValue: 'W trakcie'},
+    {value: 1, viewValue: 'Zakończony'},
+    {value: 2, viewValue: 'Problematyczny'},
+    {value: 3, viewValue: 'Skasowany'}
+  ];
   constructor() { }
 
   ngOnInit() {
