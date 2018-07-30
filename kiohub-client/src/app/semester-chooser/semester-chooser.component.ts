dc68365580945bc3fd5beae8cbea448bda91b056
Anna Ojdowska <annaojdowska@gmail.com>
2018-07-30 22:07:25 +0200

    First draft of semester chooser, simple pagination, no styles

--- kiohub-client/src/app/semester-chooser/semester-chooser.component.ts
+++ kiohub-client/src/app/semester-chooser/semester-chooser.component.ts
@@ -10,10 +10,32 @@ import { SemesterService } from './semester-service';
 })
 export class SemesterChooserComponent implements OnInit {
   semesters: Semester[];
+  pageScope = 12;
+  beginIndex = 0;
+  endIndex = this.beginIndex + this.pageScope;
+  numberOfPages = 51;
+  pathToLeftArrow = '../../assets/left-arrow.png';
+  pathToRightArrow = '../../assets/right-arrow.png';
+  page = 0;
+
   constructor(@Inject(SemesterService) private semesterService: SemesterService)  { }
 
   ngOnInit() {
     this.semesterService.getSemesters().subscribe(result => this.semesters = result);
   }
 
+  getSemestersToDisplay(begin: number, end: number) {
+    return this.semesters.slice(begin, end);
+  }
+
+  moveToNext() {
+    this.beginIndex += this.pageScope;
+    this.endIndex += this.pageScope;
+    this.page += 1;
+    console.log(this.page);
+  }
+  moveToPrevious() {
+    this.beginIndex -= this.pageScope;
+    this.endIndex -= this.pageScope;
+  }
 }
