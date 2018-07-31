c384deaea2d2338d7364b0b31a13ab55be6c578a
Anna Ojdowska <annaojdowska@gmail.com>
2018-07-31 21:16:27 +0200

    No routing for semester chooser component

--- kiohub-client/src/app/app-routing.module.ts
+++ kiohub-client/src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { SearchInputComponent } from './search-input/search-input.component';
 import { UnloggedSearchComponent } from './unlogged-search/unlogged-search.component';
 import { AddProjectComponent } from './add-project/add-project.component';
 import { EditProjectTabComponent } from './edit-project-tab/edit-project-tab.component';
-import { SemesterChooserComponent } from './semester-chooser/semester-chooser.component';
 
 
 const routes: Routes = [
   { path: 'home', component: UnloggedSearchComponent },
   { path: 'add-project', component: AddProjectComponent },
   { path: 'edit-project', component: EditProjectTabComponent },
-  { path: 'semester', component: SemesterChooserComponent },
   { path: '**', component: UnloggedSearchComponent }
 ];
 
