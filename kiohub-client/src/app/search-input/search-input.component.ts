875ecebf797e55f0ca95f6dd6372bae1d26d0c22
Anna Ojdowska <annaojdowska@gmail.com>
2018-08-18 19:59:05 +0200

    Displaying filtered possible search results in search input component

--- kiohub-client/src/app/search-input/search-input.component.ts
+++ kiohub-client/src/app/search-input/search-input.component.ts
@@ -3,7 +3,7 @@ import { FormControl } from '@angular/forms';
 import { SearchService } from '../search-service/search.service';
 import { debounceTime } from 'rxjs/operators';
 import { Project } from '../model/project.interface';
-import { Observable } from 'rxjs';
+import { Router } from '../../../node_modules/@angular/router';
 
 @Component({
 selector: 'app-search-input',
@@ -14,12 +14,21 @@ styleUrls: ['./search-input.component.css']
 export class SearchInputComponent implements OnInit {
   results: Project[] = [];
   queryField: FormControl = new FormControl();
-  filteredOptions: Observable<Project[]>;
-constructor(@Inject(SearchService) private searchService: SearchService) { }
+
+constructor(@Inject(SearchService) private searchService: SearchService, @Inject(Router) private router: Router) { }
 
 ngOnInit() {
   this.queryField.valueChanges
-  .pipe(debounceTime(50))
-  .subscribe(queryField => this.searchService.search(queryField).subscribe(res => this.results = res));
+  .pipe(debounceTime(100))
+  .subscribe(queryField => this.searchService.search(queryField).subscribe(res => this.mapResults(res, queryField)));
+ }
+
+ goToSearchResults() {
+  this.router.navigate(['/projects-base']);
+ }
+ mapResults(res: Project[], phrase: string) {
+  this.results = res;
+  // console.log('d' + phrase + 'd');
+  // console.log(this.results);
  }
 }
--- kiohub-client/src/app/search-service/search.service.ts
+++ kiohub-client/src/app/search-service/search.service.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class SearchService {
-  chosenResult: any;
   searchResults: Observable<Project[]>;
   constructor(@Inject(HttpClient) private http: HttpClient) {
     this.searchResults = new Observable<Project[]>();
@@ -15,15 +14,8 @@ export class SearchService {
     const params = new HttpParams().set('phrase', phrase);
     this.searchResults = this.http.get<Project[]>('http://kiohub.eti.pg.gda.pl:8080/project/quick-search',
     {responseType: 'json', params: params});
-    // this.searchResults = this.http.get<Project[]>('../../assets/projectExample.code-workspace', {responseType: 'json'});
      return this.searchResults;
   }
-
-  getChosenSearchResult() {
-    return this.chosenResult;
-  }
-
-  setChosenSearchResult() {} // todo: after click on chosen search results
 
   getSearchResults() {
     return this.searchResults;
