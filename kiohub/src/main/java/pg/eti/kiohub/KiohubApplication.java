f1a3df0e69e6d2604bcf80b9af80025d370423b1
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-09-28 00:03:58 +0200

    Add filters in onStartupMethod instead of web.xml

--- kiohub/src/main/java/pg/eti/kiohub/KiohubApplication.java
+++ kiohub/src/main/java/pg/eti/kiohub/KiohubApplication.java
@@ -1,5 +1,8 @@
 package pg.eti.kiohub;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -18,5 +21,24 @@ public class KiohubApplication extends SpringBootServletInitializer{
         @Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(KiohubApplication.class);
-	}                
+	}        
+
+        @Override
+        public void onStartup(ServletContext sc) {
+            FilterRegistration.Dynamic authentication = sc.addFilter("CAS Authentication Filter", org.jasig.cas.client.authentication.AuthenticationFilter.class);
+            authentication.setInitParameter("casServerLoginUrl", "https://logowanie.pg.gda.pl/login");
+            authentication.setInitParameter("serverName", "http://kiohub.eti.pg.gda.pl");
+            authentication.addMappingForUrlPatterns(null, false, "/*");
+            
+            FilterRegistration.Dynamic validation = sc.addFilter("CAS Validation Filter", org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter.class);
+            validation.setInitParameter("casServerUrlPrefix", "https://logowanie.pg.gda.pl");
+            validation.setInitParameter("serverName", "http://kiohub.eti.pg.gda.pl");
+            validation.setInitParameter("redirectAfterValidation", "true");
+            validation.setInitParameter("useSession", "true");
+            validation.setInitParameter("authn_method", "mfa-duo");
+            validation.addMappingForUrlPatterns(null, false, "/*");
+            
+            FilterRegistration.Dynamic wrapper = sc.addFilter("CAS HttpServletRequest Wrapper Filter", org.jasig.cas.client.util.HttpServletRequestWrapperFilter.class);
+            wrapper.addMappingForUrlPatterns(null, false, "/*");
+        }
 }
