80047a4e02ece1f41fb59c88e0e70f9565524b1e
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-09-28 18:19:51 +0200

    Try handle CAS response 6

--- kiohub/src/main/java/pg/eti/kiohub/controller/LoginController.java
+++ kiohub/src/main/java/pg/eti/kiohub/controller/LoginController.java
@@ -1,32 +1,32 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package pg.eti.kiohub.controller;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pg.eti.kiohub.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 ///**
 // *
 // * @author Tomasz
 // */
 //
-////@Controller
-////@RequestMapping(path = "/login")
-////public class LoginController extends MainController {
-////
-////    @RequestMapping()
-////    public ResponseEntity metoda1(HttpServletRequest request) {
-////        return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
-////    }
+@Controller
+@RequestMapping(path = "/login")
+public class LoginController extends MainController {
+
+    @RequestMapping()
+    public ResponseEntity metoda1(HttpServletRequest request) {
+        return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
+    }
 //    
 ////    @GetMapping()
 ////    public ResponseEntity metoda2(HttpServletRequest servletRequest) {
@@ -37,5 +37,5 @@
 ////    public ResponseEntity metoda3(HttpServletResponse response) {
 ////        return new ResponseEntity<>(HttpStatus.CONFLICT);
 ////    }
-//}
+}
 
