a0fbf4e85ce1bc00d50dc2a8517673f52cca67c2
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-09-28 18:11:25 +0200

    Try handle CAS response 4

--- kiohub/src/main/java/pg/eti/kiohub/controller/LoginController.java
+++ kiohub/src/main/java/pg/eti/kiohub/controller/LoginController.java
@@ -1,42 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pg.eti.kiohub.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-/**
- *
- * @author Tomasz
- */
-
-@Controller
-@RequestMapping(path = "/login")
-public class LoginController extends MainController {
-
-    @CrossOrigin
-    @RequestMapping()
-    public ResponseEntity metoda1(HttpServletRequest request) {
-        return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
-    }
-    
-//    @GetMapping()
-//    public ResponseEntity metoda2(HttpServletRequest servletRequest) {
-//        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//    }
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package pg.eti.kiohub.controller;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+///**
+// *
+// * @author Tomasz
+// */
+//
+////@Controller
+////@RequestMapping(path = "/login")
+////public class LoginController extends MainController {
+////
+////    @RequestMapping()
+////    public ResponseEntity metoda1(HttpServletRequest request) {
+////        return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
+////    }
 //    
-//    @GetMapping()
-//    public ResponseEntity metoda3(HttpServletResponse response) {
-//        return new ResponseEntity<>(HttpStatus.CONFLICT);
-//    }
-}
+////    @GetMapping()
+////    public ResponseEntity metoda2(HttpServletRequest servletRequest) {
+////        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+////    }
+////    
+////    @GetMapping()
+////    public ResponseEntity metoda3(HttpServletResponse response) {
+////        return new ResponseEntity<>(HttpStatus.CONFLICT);
+////    }
+//}
 
