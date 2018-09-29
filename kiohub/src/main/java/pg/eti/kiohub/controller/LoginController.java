3711b00a3336d6fc0d402fe8ac88662fc74fc31f
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-09-29 11:20:07 +0200

    CAS - LoginControllerFix

--- kiohub/src/main/java/pg/eti/kiohub/controller/LoginController.java
+++ kiohub/src/main/java/pg/eti/kiohub/controller/LoginController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pg.eti.kiohub.entity.model.Project;
