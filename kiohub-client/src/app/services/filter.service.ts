1facf953f9f6a552240633d2e584e84d3ad1fd27
Anna Ojdowska <annaojdowska@gmail.com>
2018-11-10 15:30:10 +0100

    Nullchecks in filter service

--- kiohub-client/src/app/services/filter.service.ts
+++ kiohub-client/src/app/services/filter.service.ts
@@ -11,51 +11,56 @@ export class FilterService {
     filterBasedOnQuery(query: QueryDescription, projects: Project[]): Project[] {
         let projectsToFilter = projects;
         query.titles.forEach(phrase => {
-            projectsToFilter = projectsToFilter.filter(p => p.title.toLowerCase().includes(phrase.toLowerCase()));
+            projectsToFilter = projectsToFilter
+                .filter(p => p.title && p.title.toLowerCase().includes(phrase.toLowerCase()));
         });
         if (projectsToFilter.length === 0) {
             return projectsToFilter;
         }
         query.tags.forEach(tag => {
-            projectsToFilter = projectsToFilter.filter(p => p.tags.map(t => t.name.toLowerCase()).includes(tag.toLowerCase()));
+            projectsToFilter = projectsToFilter
+                .filter(p => p.tags && p.tags.map(t => t.name.toLowerCase()).includes(tag.toLowerCase()));
         });
         if (projectsToFilter.length === 0) {
             return projectsToFilter;
         }
         query.licencesIds.forEach(licenceId => {
-            projectsToFilter = projectsToFilter.filter(p => p.licence.id === licenceId);
+            projectsToFilter = projectsToFilter.filter(p => p.licence && p.licence.id === licenceId);
         });
         if (projectsToFilter.length === 0) {
             return projectsToFilter;
         }
         query.projectTypesIds.forEach(typeId => {
-            projectsToFilter = projectsToFilter.filter(p => p.projectType.id === typeId);
+            projectsToFilter = projectsToFilter.filter(p => p.projectType && p.projectType.id === typeId);
         });
         if (projectsToFilter.length === 0) {
             return projectsToFilter;
         }
         query.statusesIds.forEach(statusId => {
-            projectsToFilter = projectsToFilter.filter(p => p.projectStatus.id === statusId);
+            projectsToFilter = projectsToFilter.filter(p => p.projectStatus && p.projectStatus.id === statusId);
         });
         if (projectsToFilter.length === 0) {
             return projectsToFilter;
         }
         query.semestersIds.forEach(semesterId => {
-            projectsToFilter = projectsToFilter.filter(p => p.semesters.map(semester => semester.id).includes(semesterId));
+            projectsToFilter = projectsToFilter
+                .filter(p => p.semesters && p.semesters.map(semester => semester.id).includes(semesterId));
         });
         if (projectsToFilter.length === 0) {
             return projectsToFilter;
         }
         if (!this.valueUtils.isNullOrUndefined(query.dateFrom)) {
             const date = new Date(query.dateFrom);
-            projectsToFilter = projectsToFilter.filter(p => new Date(p.publicationDate).getTime() >= date.getTime());
+            projectsToFilter = projectsToFilter
+                .filter(p => p.publicationDate && new Date(p.publicationDate).getTime() >= date.getTime());
         }
         if (projectsToFilter.length === 0) {
             return projectsToFilter;
         }
         if (!this.valueUtils.isNullOrUndefined(query.dateTo)) {
             const date = new Date(query.dateTo);
-            projectsToFilter = projectsToFilter.filter(p => new Date(p.publicationDate).getTime() <= date.getTime());
+            projectsToFilter = projectsToFilter
+                .filter(p => p.publicationDate && new Date(p.publicationDate).getTime() <= date.getTime());
         }
         return projectsToFilter;
     }
