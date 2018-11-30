a6a0d05599118cde4f11a8b18d09e45a13ca4d2b
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-11-30 21:14:34 +0100

    ErrorController #3

---
+++ kiohub/src/main/java/pg/eti/kiohub/controller/MyErrorController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pg.eti.kiohub.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.web.servlet.error.ErrorController;
+
+/**
+ *
+ * @author Tomasz
+ */
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+  @RequestMapping("/error")
+  @ResponseBody
+  public String handleError(HttpServletRequest request) {
+      return "redirect:http://kiohub.eti.pg.gda.pl/my-projects";
+  }
+
+  public String getErrorPath() {
+      return "/error";
+  }
+}
