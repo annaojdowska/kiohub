b9338d149b11f06d6473561d07fe6321e8954bb3
Anna Ojdowska <annaojdowska@gmail.com>
2018-12-03 00:09:19 +0100

    Seriously

--- kiohub/src/main/java/pg/eti/kiohub/controller/ProjectController.java
+++ kiohub/src/main/java/pg/eti/kiohub/controller/ProjectController.java
@@ -22,6 +22,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @JBossLog
+@CrossOrigin
 @Controller
 @RequestMapping(path = "/project")
 public class ProjectController extends MainController {
